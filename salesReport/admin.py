696ae249646b845bed96503fadf592d24cdde697
akiokio <akio.xd@gmail.com>
2013-05-12 22:49:58 -0300

    add admin filters

--- salesReport/admin.py
+++ salesReport/admin.py
@@ -3,6 +3,11 @@ __author__ = 'akiokio'
 from django.contrib import admin
 from salesReport.models import order, orderItem, brands
 
-admin.site.register(order)
+class orderAdmin(admin.ModelAdmin):
+    list_display = ['increment_id', 'status', 'created_at', 'grand_total']
+    list_filter = ['status', 'created_at']
+    search_fields = ['increment_id', 'status', 'created_at', 'grand_total']
+
+admin.site.register(order, orderAdmin)
 admin.site.register(orderItem)
 admin.site.register(brands)
