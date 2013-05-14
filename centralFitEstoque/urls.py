8971273295a61f4d0337eeab85e3c4842503003a
akiokio <akio.xd@gmail.com>
2013-05-14 00:56:15 -0300

    add grappelli

--- centralFitEstoque/urls.py
+++ centralFitEstoque/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^importOrdersSinceDay/(?P<dateStart>[\w|\W]+)/(?P<dateEnd>[\w|\W]+)/$', 'salesReport.views.importOrdersSinceDay', name='importOrders'),
